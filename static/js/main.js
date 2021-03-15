cc04506a58bce85f5e19574675ddf5fb1baa363c
oxbffff <off0xbffff@gmail.com>
2021-03-15 20:54:17 +0200

    added interactive with js

--- static/js/main.js
+++ static/js/main.js
@@ -1,48 +1,74 @@
-let socket = new WebSocket("ws://localhost:12345/ws");
+const socket = new WebSocket("ws://localhost:12345/ws");
+const messageInput = document.getElementById("messageInput");
+messageInput.addEventListener("keyup", ({key}) => {
+    if (key === "Enter") {
+        sendMessage();
+    }
+});
+
 let username;
 
 function sendMessage() {
-    let form = document.forms.messageForm;
-    let text = form.elements.messageInput.value;
-    form.elements.messageInput.value = "";
+    let text = messageInput.value;
+    if (text === "")  {
+        return;
+    }
+    messageInput.value = "";
 
     let date = new Date().toLocaleTimeString(
         'en-US', {
-            hour12: false,
+        hour12: false,
         hour: "numeric",
         minute: "numeric"
     });
-    
-    updateMessageContainer(`${date}: ${"You"}</br>${text}`)
 
     let message = {
         text: text,
         from_user: username,
         date: date
     }
+
+    updateMessageContainer(message);
+
     socket.send(JSON.stringify(message))
 }
 
 function updateMessageContainer(message) {
     let messageContainer = document.getElementById("messages");
-    let newMessage = document.createElement("p");
-    newMessage.innerHTML = message;
-    messageContainer.appendChild(newMessage);
+    let kindOfMessage = (message.from_user == username ? "outgoingMessage" : "incomingMessage");
+    let photo = (
+        message.from_user == username 
+        ? "../static/images/profile_pic/photo_2021-03-12 00.39.30.jpeg" 
+        : "../static/images/profile_pic/photo_2021-03-13 14.44.40.jpeg"
+    );
+
+    messageContainer.innerHTML += `<div class="${kindOfMessage}">
+    <!-- ${kindOfMessage} -->
+    <div class="${kindOfMessage}Item">
+        <img class="${kindOfMessage}ProfilePic"
+            src="${photo}">
+        <div class="${kindOfMessage}Text">
+            <div class="${kindOfMessage}Login">${message.from_user}</div>
+            ${message.text}
+        </div>
+        <div class="messageTime">${message.date}</div>
+    </div>
+</div>`
 }
 
-function initWebSocketConn() {
+function createWebSocketEvents() {
     socket.onopen = function (e) {
         alert("Join to chat");
     };
     socket.onmessage = function (e) {
-        let msg = JSON.parse(e.data);
-        console.log(msg);
-        if ("username" in msg) {
-            username = msg.username;
+        let message = JSON.parse(e.data);
+
+        if ("username" in message) {
+            username = message.username;
             return;
         }
-        updateMessageContainer(`${msg.date}: ${msg.from_user}</br>${msg.text}`)
+        updateMessageContainer(message)
     };
 }
 
-window.onload = initWebSocketConn;
+window.onload = createWebSocketEvents;
