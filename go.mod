41f1a06ba16b0b8b75ad9ecf91f66f84531b6108
oxbffff <off0xbffff@gmail.com>
2021-03-29 22:05:05 +0300

    fixed bugs

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.16
 require (
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
-	github.com/gobwas/ws v1.0.4 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/gobwas/ws v1.0.4
+	github.com/sirupsen/logrus v1.8.1
 	golang.org/x/sys v0.0.0-20210309074719-68d13333faf2 // indirect
 )
--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 const socket = new WebSocket("ws://localhost:12345/ws");
 const messageInput = document.getElementById("messageInput");
-messageInput.addEventListener("keyup", ({key}) => {
+messageInput.addEventListener("keyup", ({ key }) => {
     if (key === "Enter") {
         sendMessage();
     }
@@ -10,17 +10,17 @@ let username;
 
 function sendMessage() {
     let text = messageInput.value;
-    if (text === "")  {
+    if (text === "") {
         return;
     }
     messageInput.value = "";
 
     let date = new Date().toLocaleTimeString(
         'en-US', {
-        hour12: false,
-        hour: "numeric",
-        minute: "numeric"
-    });
+            hour12: false,
+            hour: "numeric",
+            minute: "numeric"
+        });
 
     let message = {
         text: text,
@@ -34,12 +34,12 @@ function sendMessage() {
 }
 
 function updateMessageContainer(message) {
-    let messageContainer = document.getElementById("messages");
+    let messageContainer = document.getElementsByClassName("chat")[0];
     let kindOfMessage = (message.from_user === username ? "outgoingMessage" : "incomingMessage");
     let photo = (
-        message.from_user === username
-        ? "../static/images/profile_pic/photo_2021-03-12 00.39.30.jpeg" 
-        : "../static/images/profile_pic/photo_2021-03-13 14.44.40.jpeg"
+        message.from_user === username ?
+        "../static/images/profile_pic/photo_2021-03-12 00.39.30.jpeg" :
+        "../static/images/profile_pic/photo_2021-03-13 14.44.40.jpeg"
     );
 
     messageContainer.innerHTML += `<div class="${kindOfMessage}">
@@ -59,10 +59,10 @@ function updateMessageContainer(message) {
 }
 
 function createWebSocketEvents() {
-    socket.onopen = function (e) {
+    socket.onopen = function(e) {
         alert("Join to chat");
     };
-    socket.onmessage = function (e) {
+    socket.onmessage = function(e) {
         let message = JSON.parse(e.data);
 
         if ("username" in message) {
