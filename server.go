95c61ce2dd221297c038d92b3237703b1c17c3cf
oxbffff <off0xbffff@gmail.com>
2021-04-10 20:41:49 +0300

    Fixed code

--- server.go
+++ server.go
@@ -2,10 +2,45 @@ package main
 
 import (
 	"html/template"
+	"io"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gobwas/ws"
+	"github.com/sirupsen/logrus"
 )
+
+var (
+	logger  *logrus.Logger
+	logFile *os.File
+)
+
+func init() {
+	var err error
+	logFile, err = os.OpenFile("logs.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		log.Fatalf("Error opening log file: %s", err.Error())
+	}
+
+	logger = logrus.New()
+	logger.Formatter = &logrus.JSONFormatter{}
+	logger.SetOutput(io.MultiWriter(os.Stdout, logFile))
+
+	logger.Info("Logging started")
+}
+
+func main() {
+	defer logFile.Close()
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", chatPage)
+	http.HandleFunc("/ws", upgradeToWebSocketConn)
+
+	go eventHandler()
+
+	logger.Panic(http.ListenAndServe(":12345", nil))
+}
 
 func upgradeToWebSocketConn(w http.ResponseWriter, r *http.Request) {
 	conn, _, _, err := ws.UpgradeHTTP(r, w)
@@ -26,16 +61,4 @@ func chatPage(w http.ResponseWriter, _ *http.Request) {
 		logger.Error(err)
 		http.Error(w, "Error while parsing html file", http.StatusInternalServerError)
 	}
-}
-
-func main() {
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.HandleFunc("/", chatPage)
-	http.HandleFunc("/ws", upgradeToWebSocketConn)
-
-	go eventHandler()
-
-	logger.Error(http.ListenAndServe(":12345", nil))
 }
