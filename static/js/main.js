49290f430f242c528bcad3098834136c275a5755
Shipiyaka <alina.nagornaya49@gmail.com>
2021-07-02 13:48:56 +0300

    html, css changes

--- static/js/main.js
+++ static/js/main.js
@@ -111,3 +111,24 @@ function uploadFile() {
   }
 
 window.onload = createWebSocketEvents;
+/*
+$(function(){
+    $('.minimized').click(function(event) {
+        var i_path = $(this).attr('src');
+        $('body').append('<div id="overlay"></div><div id="magnify"><img src="'+i_path+'"><div id="close-popup"><i></i></div></div>');
+        $('#magnify').css({
+            left: ($(document).width() - $('#magnify').outerWidth())/2,   
+            top: ($(window).height() - $('#magnify').outerHeight())/2
+        });
+        $('#overlay, #magnify').fadeIn('fast');
+    });
+    
+    $('body').on('click', '#close-popup, #overlay', function(event) {
+      event.preventDefault();
+  
+      $('#overlay, #magnify').fadeOut('fast', function() {
+        $('#close-popup, #magnify, #overlay').remove();
+      });
+    });
+  });
+  */
